67057afc3aa6345b60680a12320ccf66e10cb9f1
David <dshardman@outlook.com>
2021-04-27 11:18:20 +0100

    added script to remove skin generated by cura

---
+++ RemoveSkin.py
@@ -0,0 +1,27 @@
+filestub = "test"
+filename = filestub + '.gcode'
+file = open(filename, 'r')
+lines = file.readlines()
+
+file.seek(0)
+n = 0
+line = file.readline()
+while 1:
+  if line == ';TYPE:SKIN\n':
+    skinstart = n
+  elif line == 'G1 Z40 ;Raise Z\n':
+    skinend = n - 1
+  elif line == '':
+    break
+  n += 1
+  line = file.readline()
+file.close()
+
+file = open(filename, 'w')
+n = 0
+for line in lines:
+  if n < skinstart or n > skinend:
+    file.write(line)
+  n += 1
+
+file.close()
